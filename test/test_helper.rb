b839dad64aea6e42cea814a2621b6961b574ce9d
Marius Pop <marius@mlpinit.com>
2016-01-15 14:31:09 -0800

    use minitest reporters for coloration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'pry'
-require 'minitest/pride'
+require 'minitest/reporters'
 
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new({ color: true })]
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
