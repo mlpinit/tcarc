05744314496fcd2316d02e96a820a4565af0ecd3
Marius Pop <marius@mlpinit.com>
2016-01-16 18:34:31 -0800

    add castle and field connection validation

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -4,25 +4,25 @@ class Tile < ActiveRecord::Base
 
   def road_east_to_west
     if east_neighbour && self.east != east_neighbour.west
-      errors.add(:east, "road cannot be attached to west non-road")
+      errors.add(:east, "#{east} cannot be attached to west non-#{east}")
     end
   end
 
   def road_west_to_east
     if west_neighbour && self.west != west_neighbour.east
-      errors.add(:west, "road cannot be attached to east non-road")
+      errors.add(:west, "#{west} cannot be attached to east non-#{west}")
     end
   end
 
   def road_north_to_south
     if north_neighbour && self.north != north_neighbour.south
-      errors.add(:north, "road cannot be attached to south non-road")
+      errors.add(:north, "#{north} cannot be attached to south non-#{north}")
     end
   end
 
   def road_south_to_north
     if south_neighbour && self.south != south_neighbour.north
-      errors.add(:south, "road cannot be attached to north non-road")
+      errors.add(:south, "#{south} cannot be attached to north non-#{south}")
     end
   end
 
--- test/models/title_test.rb
+++ test/models/title_test.rb
@@ -54,4 +54,108 @@ class TileTest < ActiveSupport::TestCase
       "South road cannot be attached to north non-road"
   end
 
+  test "allow castle attach from east to west" do
+    tile = Tile.new(x: 1, y: 2, east: "castle")
+    tile.valid?
+    assert tile.errors.empty?
+  end
+
+  test "don't allow castle attach to non-castle from east to west" do
+    tile = Tile.new(x: -1, y: 0, east: "castle")
+    tile.valid?
+    assert_equal tile.errors.full_messages.first, 
+      "East castle cannot be attached to west non-castle"
+  end
+
+  test "allow castle attach from west to east" do
+    tile = Tile.new(x: 3, y: 2, west: "castle")
+    tile.valid?
+    assert tile.errors.empty?
+  end
+
+  test "don't allow castle attach to non-castle from west to east" do
+    tile = Tile.new(x: 5, y: 4, west: "castle")
+    tile.valid?
+    assert_equal tile.errors.full_messages.first, 
+      "West castle cannot be attached to east non-castle"
+  end
+
+  test "allow castle attach from north to south" do
+    tile = Tile.new(x: 2, y: 1, north: "castle")
+    tile.valid?
+    assert tile.errors.empty?
+  end
+
+  test "don't allow castle attach to non-castle from north to south" do
+    tile = Tile.new(x: 0, y: -1, north: "castle")
+    tile.valid?
+    assert_equal tile.errors.full_messages.first, 
+      "North castle cannot be attached to south non-castle"
+  end
+
+  test "allow castle attach from south to north" do
+    tile = Tile.new(x: 2, y: 3, south: "castle")
+    tile.valid?
+    assert tile.errors.empty?
+  end
+
+  test "don't allow castle attach to non-castle from south to north" do
+    tile = Tile.new(x: 0, y: 1, south: "castle")
+    tile.valid?
+    assert_equal tile.errors.full_messages.first, 
+      "South castle cannot be attached to north non-castle"
+  end
+
+  test "allow field attach from east to west" do
+    tile = Tile.new(x: 3, y: 4, east: "field")
+    tile.valid?
+    assert tile.errors.empty?
+  end
+
+  test "don't allow field attach to non-field from east to west" do
+    tile = Tile.new(x: 1, y: 2, east: "field")
+    tile.valid?
+    assert_equal tile.errors.full_messages.first, 
+      "East field cannot be attached to west non-field"
+  end
+
+  test "allow field attach from west to east" do
+    tile = Tile.new(x: 5, y: 4, west: "field")
+    tile.valid?
+    assert tile.errors.empty?
+  end
+
+  test "don't allow field attach to non-field from west to east" do
+    tile = Tile.new(x: 1, y: 0, west: "field")
+    tile.valid?
+    assert_equal tile.errors.full_messages.first, 
+      "West field cannot be attached to east non-field"
+  end
+
+  test "allow field attach from north to south" do
+    tile = Tile.new(x: 4, y: 3, north: "field")
+    tile.valid?
+    assert tile.errors.empty?
+  end
+
+  test "don't allow field attach to non-field from north to south" do
+    tile = Tile.new(x: 0, y: -1, north: "field")
+    tile.valid?
+    assert_equal tile.errors.full_messages.first, 
+      "North field cannot be attached to south non-field"
+  end
+
+  test "allow field attach from south to north" do
+    tile = Tile.new(x: 4, y: 5, south: "field")
+    tile.valid?
+    assert tile.errors.empty?
+  end
+
+  test "don't allow field attach to non-field from south to north" do
+    tile = Tile.new(x: 0, y: 1, south: "field")
+    tile.valid?
+    assert_equal tile.errors.full_messages.first, 
+      "South field cannot be attached to north non-field"
+  end
+
 end
