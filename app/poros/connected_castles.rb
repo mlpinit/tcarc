c82fe1f4131d0c72b8076236dd7c895de08db023
Marius Pop <marius@mlpinit.com>
2016-02-15 14:52:57 -0800

    add connected castles

--- app/poros/connected_castles.rb
+++ app/poros/connected_castles.rb
@@ -1,5 +1,40 @@
 class ConnectedCastles
-  # go all directions recursively until already visited or no tile exists
-  # + keep track of tiles
-  # + keep track of open ended neighbours
+  include TileConnection
+
+  attr_reader :game_tiles, :meeple, :connections
+
+  def initialize(game_tiles:, meeple:)
+    @game_tiles = game_tiles
+    @meeple = meeple
+    @connections = []
+  end
+
+  def connections_composite_keys
+    extract(current_tile, meeple.direction)
+    connections
+  end
+
+  private
+
+  def extract(tile, direction)
+    if tile.connected_castle
+      collect_all_connections_ids(tile)
+    else
+      collect_single_connection_id(tile, direction)
+    end
+  end
+
+  def collect_all_connections_ids(tile)
+    castles_directions = directions.select { |dir| tile.send(dir) == "castle" }
+    castles_directions.each { |dir| collect_single_connection_id(tile, dir) }
+  end
+
+  def collect_single_connection_id(tile, direction)
+    composite_key = [tile.id, direction]
+    return if connections.include?(composite_key)
+    connections << composite_key 
+    neighbour = neighbour(tile.x, tile.y, direction)
+    extract(neighbour, opposite_direction(direction)) if neighbour
+  end
+
 end
---
+++ test/poros/connected_castles_test.rb
@@ -0,0 +1,113 @@
+require 'test_helper'
+
+class ConnectedCastlesTest < ActiveSupport::TestCase
+
+  test 'same tile castle not connected' do
+    assert_equal game_tiles1_expected, ConnectedCastles
+      .new(game_tiles: game_tiles1, meeple: meeple)
+      .connections_composite_keys
+  end
+
+  test 'same tile connected castle' do
+    assert_equal game_tiles2_expected, ConnectedCastles
+      .new(game_tiles: game_tiles2, meeple: meeple)
+      .connections_composite_keys 
+      .sort
+  end
+
+  test "connected and not connected combo" do
+    assert_equal game_tiles3_expected, ConnectedCastles
+      .new(game_tiles: game_tiles3, meeple: meeple)
+      .connections_composite_keys
+      .sort
+  end
+
+  test "fully closed castle" do
+    assert_equal game_tiles4_expected, ConnectedCastles
+      .new(game_tiles: game_tiles4, meeple: meeple)
+      .connections_composite_keys 
+      .sort
+  end
+
+  private
+
+  def meeple
+    OpenStruct.new(tile_id: 0, direction: "north")
+  end
+
+  def game_tiles1
+    [
+      OpenStruct.new(
+        id: 0, x: 0, y: 0, connected_castle: false,
+        south: "field", west: "field", north: "castle", east: "castle")
+    ]
+  end
+
+  def game_tiles1_expected
+    [[0, "north"]]
+  end
+
+  def game_tiles2
+    [
+      OpenStruct.new(
+        id: 0, x: 0, y: 0, connected_castle: true,
+        south: "field", west: "field", north: "castle", east: "castle")
+    ]
+  end
+
+  def game_tiles3
+    [
+      OpenStruct.new(
+        id: 0, x: 0, y: 0, connected_castle: false,
+        south: "field", west: "field", north: "castle", east: "castle"),
+      OpenStruct.new(
+        id: 1, x: 0, y: 1, connected_castle: true,
+        south: "field", west: "field", south: "castle", east: "castle"),
+      OpenStruct.new(
+        id: 2, x: 1, y: 1, connected_castle: true,
+        south: "field", west: "castle", south: "castle", east: "field", north: "castle")
+    ]
+  end
+
+  def game_tiles4
+    [
+      OpenStruct.new(id: 0, x: 0, y: 0, connected_castle: true,
+        south: "castle", west: "castle", north: "castle", east: "castle"),
+      OpenStruct.new(id: 1, x: 1, y: 0, connected_castle: false,
+        south: "field", west: "castle", north: "field", east: "field"),
+      OpenStruct.new(id: 2, x: 0, y: 1, connected_castle: false,
+        south: "castle", west: "field", north: "field", east: "field"),
+      OpenStruct.new(id: 3, x: 0, y: -1, connected_castle: false,
+        south: "field", west: "field", north: "castle", east: "field"),
+      OpenStruct.new(id: 4, x: -1, y: 0, connected_castle: true,
+        south: "field", west: "castle", north: "field", east: "castle"),
+      OpenStruct.new(id: 5, x: -2, y: 0, connected_castle: true,
+        south: "field", west: "field", north: "castle", east: "castle"),
+      OpenStruct.new(id: 6, x: -2, y: 1, connected_castle: false,
+        south: "castle", west: "field", north: "field", east: "field")
+    ]
+  end
+
+  def game_tiles2_expected
+    [[0, "east"], [0, "north"]].sort
+  end
+
+  def game_tiles3_expected
+    [
+      [0, "north"],
+      [1, "east"], [1, "south"],
+      [2, "west"], [2, "north"], [2, "south"]
+    ].sort
+  end
+
+  def game_tiles4_expected
+    [
+      [0, "north"], [0, "south"], [0, "west"], [0, "east"],
+      [1, "west"], [2, "south"], [3, "north"],
+      [4, "west"], [4, "east"],
+      [5, "east"], [5, "north"],
+      [6, "south"]
+    ].sort
+  end
+
+end
