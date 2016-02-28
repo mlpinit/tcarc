b9e336dda735bd668a7d979b7ff92053e69b87fd
Marius Pop <marius@mlpinit.com>
2016-02-28 14:27:06 -0800

    use class_name vs class in game_player model

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -2,5 +2,5 @@ class GamePlayer < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
 
-  has_one :next_game_player, class: "GamePlayer", foreign_key: :next_game_player_id
+  has_one :next_game_player, class_name: "GamePlayer", foreign_key: :next_game_player_id
 end
