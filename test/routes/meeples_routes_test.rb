b230c658a79795e9bd54815876a5cfbc2c133eb0
Marius Pop <marius@mlpinit.com>
2016-03-09 23:28:18 -0800

    fix meeples routes test

--- test/routes/meeples_routes_test.rb
+++ test/routes/meeples_routes_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class MeeplesRoutesTest < ActionDispatch::IntegrationTest
 
   test "meeples#create" do
-    assert_routing({ method: "POST", path: "/tiles" }, { controller: "tiles", action: "create" })
+    assert_routing({ method: "POST", path: "/meeples" }, { controller: "meeples", action: "create" })
   end
 
 end
