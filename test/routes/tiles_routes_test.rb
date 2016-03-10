fd8d761239085db0bad3c9417d1afd1844c039f1
Marius Pop <marius@mlpinit.com>
2016-03-09 21:16:36 -0800

    fix test naming - users to tiles

--- test/routes/tiles_routes_test.rb
+++ test/routes/tiles_routes_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class TileRoutesTest < ActionDispatch::IntegrationTest
 
-  test "users#create" do
+  test "tiles#create" do
     assert_routing({ method: "POST", path: "/tiles" }, { controller: "tiles", action: "create" })
   end
 
