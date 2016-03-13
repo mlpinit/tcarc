2364f1c82890c0332ce0779287e50493773c32a1
Marius Pop <marius@mlpinit.com>
2016-03-12 18:03:46 -0800

    add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(email: "marius@mlpinit.com", username: "mlpinit", password: "12341234", password_confirmation: "12341234")
+User.create!(email: "tammy@mlpinit.com", username: "smurfik", password: "12341234", password_confirmation: "12341234")
+User.create!(email: "test1@mlpinit.com", username: "test1", password: "12341234", password_confirmation: "12341234")
+User.create!(email: "test2@mlpinit.com", username: "test2", password: "12341234", password_confirmation: "12341234")
+User.create!(email: "frank@mlpinit.com", username: "frank", password: "12341234", password_confirmation: "12341234")
